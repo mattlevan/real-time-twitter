a84c238364d4aabea64d7a84d6d83c13e4c27869
Matthew Levan <matt.s.levan@gmail.com>
2020-03-07 12:56:06 -0700

    Change * import to explicit imports

--- consumer/src/main/java/TwitterConsumerThread.java
+++ consumer/src/main/java/TwitterConsumerThread.java
@@ -1,4 +1,9 @@
-import org.apache.kafka.clients.consumer.*;
+import org.apache.kafka.clients.consumer.CommitFailedException;
+import org.apache.kafka.clients.consumer.Consumer;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
 import org.apache.kafka.common.serialization.LongDeserializer;
 import org.apache.kafka.common.serialization.StringDeserializer;
 import java.io.FileReader;
